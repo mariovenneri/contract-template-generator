1dd004d298fda61cbb7282c76171caa667c9b580
mvenneri42 <mvenneri42@gmail.com>
2026-08-25 13:49:29 -0400

    fix: more testing to fix externalization issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@sparticuz/chromium']
+  serverExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
+  outputFileTracingIncludes: {
+    '/api/pdf/route': ['node_modules/@sparticuz/chromium/bin/**']
+  }
 };
 
 export default nextConfig;
